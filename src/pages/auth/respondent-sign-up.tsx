c2e00fdc8b1324d7abf5354500cd2f0c1bc809e9
Jeshwanth Kumar A S <jeshwanth@thepenindia.com>
2025-10-13 12:15:40 +0530

    fix: gender issue

--- src/pages/auth/respondent-sign-up.tsx
+++ src/pages/auth/respondent-sign-up.tsx
@@ -154,6 +154,8 @@ const storeImplementation = (set: any, get: any) => ({
         errors['location.countryOrRegion'] = validationMessages.countryOrRegion;
       if (!formData.location?.zipCode?.trim())
         errors['location.zipCode'] = validationMessages.zipCode;
+      if (!formData.gender?.trim())
+        newErrors.gender = validationMessages.gender;
       // if (
       //   formData.dateOfBirth &&
       //   (!formData.dateOfBirth.month ||
