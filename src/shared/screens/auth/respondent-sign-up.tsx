e6a8dc9c95c044b4042e1d685393c47d217744b2
Jeshwanth Kumar A S <jeshwanth@thepenindia.com>
2026-03-06 17:53:39 +0530

    fix: survey-campaign

--- src/shared/screens/auth/respondent-sign-up.tsx
+++ src/shared/screens/auth/respondent-sign-up.tsx
@@ -441,7 +441,7 @@ const RespondentSignUpPage: React.FC = () => {
         // Otherwise, check for allocated survey or use default
         const allocatedSurveyId = localStorage.getItem('tm_allocated_survey');
         const redirectTo = allocatedSurveyId
-          ? `/survey-boards/${allocatedSurveyId}`
+          ? `/survey-campaign/${allocatedSurveyId}`
           : localStorage.getItem('tm_redirect_after_signup') || ROUTES.SURVEY_BOARDS;
 
         // Clean up after use
